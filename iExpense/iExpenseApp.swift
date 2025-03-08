f038beff9ac018c3b2cdf05a76bd96b41384d767
varadasainikhil <varadasainikhil@gmail.com>
2025-03-08 18:21:00 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sai Nikhil Varada on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
